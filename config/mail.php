966439428a6e6db9b85e20237007756cc749545d
Dina1993 <dina.paz@cablecolor.net>
2020-09-02 12:11:09 -0600

    stream

--- config/mail.php
+++ config/mail.php
@@ -17,6 +17,13 @@ return [
     */
 
     'driver' => env('MAIL_DRIVER', 'smtp'),
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+        ],
 
     /*
     |--------------------------------------------------------------------------
@@ -132,5 +139,7 @@ return [
     */
 
     'log_channel' => env('MAIL_LOG_CHANNEL'),
+
+  
 
 ];
